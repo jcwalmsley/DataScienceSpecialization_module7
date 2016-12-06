96ff933d35e7019ab5148dcf77911f6743aa315f
jcwalmsley <jw.prof.cntrng@gmail.com>
2016-12-06 10:00:43 +0100

    updates

--- 7)ProcessingData.R
+++ 7)ProcessingData.R
@@ -1,6 +1,12 @@
 #Processing data:
 Transformations;
-1 factor variables 8:11;
-2 change variable labels in columns 8 & 9;
-a Note; for column header 8 = vs; variable names = V-block, & S-block;
-b Note; for column header 9 = am; variable names = A-type = A, & M-type;
+        1 factor variables 8:11;
+        2 change variable labels in columns 8 & 9;
+                a Note; for column header 8 = vs; variable names = V-block, & S-block;
+                b Note; for column header 9 = am; variable names = A-type = A, & M-type;
+---
+```{r Process data, echo=FALSE}
+data(mtcars)
+mtcars$vs <- factor(mtcars$vs, labels = c("V-block", "S-block")); mtcars$am <- factor(mtcars$am, labels = c("A-type", "M-type")); mtcars$gear <- factor(mtcars$gear); mtcars$carb <- factor(mtcars$carb); head(mtcars)
+```
+---
--- 8)ExploratoryAnalysis.R
+++ 8)ExploratoryAnalysis.R
@@ -1,19 +1,113 @@
 #Exploratory analysis:
-Histograms
-Rug
-Boxplots
+---
+# YES Pairs plot
+# Figures: Exploratory see Appendix A, Figure
+```{r Pairs plot, echo=FALSE, fig.height=6, fig.width=6}
+pairs(mpg ~ ., data = mtcars)
+```
+---
+# YES Histograms
+# Figures: Exploratory see Appendix A -?
+ ```{r Hist plot, echo=FALSE, fig.height=5, fig.width=5, message=FALSE, warning=FALSE}
+data("mtcars"); library(ggplot2); library(dplyr);
+hist(mtcars$mpg, breaks=15, xlab = "Miles-Per-gallon", main = "MPG Histogram", xlim = range(5:40))
+```
+
+
+data(mtcars)
+mtcars$vs <- factor(mtcars$vs, labels = c("V-block", "S-block")); mtcars$am <- factor(mtcars$am, labels = c("Automatic", "Manual")); mtcars$gear <- factor(mtcars$gear); mtcars$carb <- factor(mtcars$carb); head(mtcars)
+par(mfrow = c(1,2), mar = c(3, 3, 2, 1))
+hist(subset(mtcars, am == "Automatic")$mpg, main = "MPG rating for Automatic Transmissions",xlab = "Miles per Gallon")
+abline(v = mean(subset(mtcars, mtcars$am == "Automatic")$mpg), col = "red", lwd = 3)
+hist(subset(mtcars, am=="Manual")$mpg, main = "MPG rating for Manual Transmissions", xlab = "Miles per Gallon")
+abline(v = mean(subset(mtcars, mtcars$am == "Manual")$mpg), col = "red", lwd =3)
+
+
+---
+YES PLOT 2 = Boxplot
+Figures: Exploratory see Appendix A -?
+```{r Box plot, echo=FALSE, fig.height=5, fig.width=5}
+boxplot(mpg ~ am, data = mtcars, col = (c("grey","grey")), ylab = "Miles-Per-Gallon", xlab = "Transmission", main =  "Transmission type ~ Miles per Gallon")
+```
+---
+YES QQ plot
+Figures: Exploratory see Appendix A -?
+```{r QQ plot, echo=FALSE, fig.height=5, fig.width=5}
+#qqPlot(fit?, main="Normal Q-Q plot")
+```
+---
+YES Fitted
+Figures: Exploratory see Appendix A -?
+
+
+
+
+
+
+
+YES Residuals
+Figures: Exploratory see Appendix A -?
+data("mtcars")
+fit <- lm(mpg ~ am, mtcars)
+fit$residuals
+summary(fit)
+mean(fit$residuals)
+cov(fit$residuals, mtcars$am)
+
+
+
+YES Residuals vs Fitted
+Figures: Exploratory see Appendix A -?
+
+
+
+
+
 Barplots
-Scatterplots
+
+
+
+YES Scatterplots
+Figures: Exploratory see Appendix A -?
+data("mtcars")
+plot(mpg ~ am, mtcars)
+plot(jitter(mpg,4) ~ am, mtcars)
+regrline <- lm(mpg ~ am, mtcars)
+abline(regrline, lwd=3, col='red')
+summary(regrline)
+
+
 Multiple plots
+
+par(mfrow = c(1, 2), mar = c(5, 4, 2, 1)) = sets margins
+with(subset(pollution, region == "west"), plot(latitude, pm25, main = "West")) =
+        left scatterplot
+with(subset(pollution, region == "east"), plot(latitude, pm25, main = "East")) =
+        right scatterplot
+
+
+
+
+
 Graphing - base, lattice, ggpplot2
+
 ABlines (h/v)
+
 Confidence intervals
+Figures: Exploratory see Appendix A -?
+
 Standard error
+
 Variance
+
 Fitted lines
+
 Heatmap
+
 K-Means
+
 Dimension Reduction
-PCA
-SVD
+        PCA
+        SVD
+
 Figures: Exploratory see Appendix A
