67a5ae088f6a9b1a9213b378ca4b3804e24643f6
jcwalmsley <jw.prof.cntrng@gmail.com>
2016-12-07 19:56:34 +0100

    updates

--- 9)StatisticalModelling.R
+++ 9)StatisticalModelling.R
@@ -3,13 +3,58 @@ Assumptions:
         A correlation exists among one or more varaibles
         B
         C
-Simple Linear Regression
+---
+
 Multivariate Linear Regression
         lm - simple
+```{r SimpleLinearRegression, echo=FALSE}
+fslrm <- lm(mpg ~ am, data = mtcars)
+coef(summary(fslrm))
+plot(fslrm)
+```
+---
+
         lm - multivariate
-        lm - nested
+```{r MLR, echo=TRUE,}
+mlr1 <- lm(mpg ~ ., data = mtcars)
+coef(summary(mlr1))
+plot(mlr1)
+```
+---
+        lm - nested / lm - step 'function'
+```{r Nested, echo=TRUE}
+data("mtcars")
+names(mtcars)
+fn1 <- lm(mpg ~ cyl + disp, data = mtcars)
+fn2 <- update(fn1, mpg ~ cyl + disp + hp)
+fn3 <- update(fn1, mpg ~ cyl + disp + hp + drat)
+fn4 <- update(fn1, mpg ~ cyl + disp + hp + drat + wt)
+fn5 <- update(fn1, mpg ~ cyl + disp + hp + drat + wt +  qsec)
+fn6 <- update(fn1, mpg ~ cyl + disp + hp + drat + wt +  qsec + vs)
+fn7 <- update(fn1, mpg ~ cyl + disp + hp + drat + wt +  qsec + vs +  am)
+fn8 <- update(fn1, mpg ~ cyl + disp + hp + drat + wt +  qsec + vs +  am +  gear)
+fn9 <- update(fn1, mpg ~ cyl + disp + hp + drat + wt +  qsec + vs +  am +  gear + carb)
+anova(fn1, fn2, fn3, fn4, fn5, fn6, fn7, fn8, fn9)
+```
+---
         lm - remove the intercept (-1)
-        lm - step function
+
+        GLM
+        Binary GLM
+```{r Binary GLM, echo=FALSE, fig.height=6, fig.width=6}
+beta0 = 0; beta1 = 1
+p = 1 / (1 + exp(-1 * (beta0 + beta1 * x)))
+
+plot(x, y, frame = FALSE, xlab = "x", ylab = "y") lines(lowess(x, y), type = "l", col = "blue", lwd = 3)
+fit = glm(y ~ x, family = binomial)
+lines(x, predict(fit, type = "response"), lwd = 3, col = "red")
+```
+
+
+
+
+
+
 Coefficients / Slope
 Standard Error
 T-Vales
@@ -27,3 +72,8 @@ Influence Measures
 Chisq
 Ancova
 *GLMs
+*Binary GLM
+
+
+
+
